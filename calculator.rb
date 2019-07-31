56f8eb7137e3e967922671bc226221e87b5c9abd
Srikalyan Megati <kalmegati@gmail.com>
2019-07-31 16:12:38 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-"first_number" = 6
+first_number = 6
 
-"second_number" = 3
+second_number = 3
 
-"sum" = "first_numbe" + "second_number"
+sum = first_numbe + second_number
